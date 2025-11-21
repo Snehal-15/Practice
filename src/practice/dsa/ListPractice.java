837674bd8116baf6a69d3f9350240a8d764e71da
Snehal-15 <72665005+Snehal-15@users.noreply.github.com>
2025-11-21 20:08:09 +0530

    Add even length words to list and print

--- src/practice/dsa/ListPractice.java
+++ src/practice/dsa/ListPractice.java
@@ -4,48 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+
+//String str = "My name is Snehal";
+//String str2 = "My name is Ruturaj";
+//split words of string and store it in array
+//use for loop to iterate through each element in string array
+//if words length is even then add to list
+//else move to the next word
 public class ListPractice {
 
     public static void main(String[] args) {
+        String firstString = "My name is Snehal";
+        String secondString = "My name is Ruturaj";
+        String[] array = secondString.split(" ");
+        List<String> list = new ArrayList<String>();
 
-        String str = "My name is Snehal";
-        String str2 = "My name is Ruturaj";
-
-        String[] array = str.split(" ");
-        int arrayLength = array.length;
-        int count = 0;
-
-        for(int i = 0; i<arrayLength; i++){
-            String currWord = array[i];
-            int lengthOfCurrWord = currWord.length();
-            if(lengthOfCurrWord % 2 == 0){
-                count++;
-            }
-            else{
-                continue;
+        for(int i=0; i<array.length; i++){
+            if(array[i].length() %2 == 0){
+                list.add(array[i]);
             }
         }
 
-        String[] list = new String[count];
-
-        for(int i=0; i<arrayLength; i++){
-            String currWord = array[i];
-            int lengthOfCurrWord = currWord.length();
-            if(lengthOfCurrWord % 2 == 0){
-                list[i] = currWord;
-            }
-            else{
-                continue;
-            }
+        for(String even : list){
+            System.out.println(even);
         }
-
-        System.out.println(Arrays.toString(list));
     }
 
-    //String str = "My name is Snehal";
-    //String str2 = "My name is Ruturaj";
-    //split words of string and store it in array
-    //use for loop to iterate through each element in string array
-    //if words length is even then add to list
-    //else move to the next word
 }
