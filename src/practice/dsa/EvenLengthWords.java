ac7d037812f45f49353968ade94cb87d7d6fd426
Snehal-15 <72665005+Snehal-15@users.noreply.github.com>
2025-11-21 19:57:43 +0530

    Even length words

--- src/practice/dsa/EvenLengthWords.java
+++ src/practice/dsa/EvenLengthWords.java
@@ -1,4 +1,17 @@
 package practice.dsa;
 
 public class EvenLengthWords {
+
+    public static void main(String args[]){
+        String sentence = "Hi this is Check of Even Length";
+        String[] words = sentence.split(" ");
+
+        for(int i=0; i<words.length; i++){
+            if(words[i].length()%2 == 0){
+                System.out.println(words[i]);
+            }
+
+        }
+
+    }
 }
